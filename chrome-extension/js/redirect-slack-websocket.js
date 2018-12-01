f8f3c6292761eceebfd4473947f177bf08202d06
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-12-01 21:06:26 +0900

    feat: Slackが使用するWebSocketのリクエストURLを書き換えるChrome拡張を作成

---
+++ chrome-extension/js/redirect-slack-websocket.js
@@ -0,0 +1,25 @@
+
+// 「windowオブジェクト・flashのメソッド」にアクセスする場合これが必要
+((fn) => {
+	const scriptElement = document.createElement('script');
+	const script = fn.toString();
+	scriptElement.appendChild(document.createTextNode(`(${script})()`));
+
+	const parent = document.head || document.body || document.documentElement;
+	parent.appendChild(scriptElement);
+	parent.removeChild(scriptElement);
+})(() => {
+	(() => {
+		class CustomizedWebSocket extends window.WebSocket {
+			constructor(...args) {
+				const url = new URL(args[0]);
+				if (url.hostname === 'wss-primary.slack.com') {
+					url.hostname = 'everything.lb.slack-msgs.com';
+					args[0] = url.toString();
+				}
+				super(...args);
+			}
+		}
+		window.WebSocket = CustomizedWebSocket;
+	})();
+});
